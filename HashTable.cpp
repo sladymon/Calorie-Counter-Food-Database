b50bd85edb926d654ff07a4513c0c6a65c1d161c
WENDY MARTELL <wendymartelll@gmail.com>
2015-06-11 19:36:09 -0700

    I deleted NON needed files

--- HashTable.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  HashTable.cpp
-//  Food_Calorie_Counter_22C
-//
-//  Created by Wendy Martell on 6/8/15.
-//  Copyright (c) 2015 ___MARTELL___. All rights reserved.
-//
-
-#include "HashTable.h"
