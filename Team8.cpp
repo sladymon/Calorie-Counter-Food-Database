8097f73930c8038b13486e847faace2264bca2c5
sladymon <shannon.ladymon@gmail.com>
2015-06-15 17:39:00 -0700

    fixed input/output file names

--- Team8.cpp
+++ Team8.cpp
@@ -4,15 +4,15 @@
 
 using namespace std;
 
-//<<<<<<< Updated upstream
 //Shannon's files
-const string INPUT_FILE = "/Users/Shannon/Documents/GitHub/Calorie-Counter-Food-Database/foodInput.txt";
-const string OUTPUT_FILE = "/Users/Shannon/Documents/GitHub/Calorie-Counter-Food-Database/foodOutput.txt";
-const string PRIME_NUMBERS = "/Users/Shannon/Documents/GitHub/Calorie-Counter-Food-Database/primeNumbers.txt";
+//const string INPUT_FILE = "/Users/Shannon/Documents/GitHub/Calorie-Counter-Food-Database/foodInput.txt";
+//const string OUTPUT_FILE = "/Users/Shannon/Documents/GitHub/Calorie-Counter-Food-Database/foodOutput.txt";
+//const string PRIME_NUMBERS = "/Users/Shannon/Documents/GitHub/Calorie-Counter-Food-Database/primeNumbers.txt";
 
 //Wendy's files
 //const string INPUT_FILE = "/Users/wendymartell/Dropbox/GITHUB/Food-Calorie-Counter-22C-2015/Calorie-Counter-Food-Database/foodInput.txt";
 //const string INPUT_FILE = "/Users/wendymartell/Dropbox/GITHUB/Food-Calorie-Counter-22C-2015/Calorie-Counter-Food-Database/foodOutput.txt";
+//const string PRIME_NUMBERS = "/Users/wendymartell/Dropbox/GITHUB/Food-Calorie-Counter-22C-2015/Calorie-Counter-Food-Database/primeNumbers.txt";
 
 //Shuti's files
 //const string INPUT_FILE = "foodInput.txt";
@@ -20,12 +20,9 @@ const string PRIME_NUMBERS = "/Users/Shannon/Documents/GitHub/Calorie-Counter-Fo
 //const string PRIME_NUMBERS = "primeNumbers.txt";
 
 //Deepika's files
-//=======
-//const string INPUT_FILE = "/Users/wendymartell/Dropbox/GITHUB/Food-Calorie-Counter-22C-2015/Calorie-Counter-Food-Database/foodInput.txt";
-//const string INPUT_FILE = "/Users/Shannon/Documents/GitHub/Calorie-Counter-Food-Database/foodInput.txt";
-//>>>>>>> Stashed changes
 //const string INPUT_FILE = "D:\\foodInput.txt";
 //const string OUTPUT_FILE = "D:\\foodOutput.txt";
+//const string OUTPUT_FILE = "D:\\primeNumbers.txt";
 
 
 int determineHashSize(const char* fileName, const char* primeFile);
